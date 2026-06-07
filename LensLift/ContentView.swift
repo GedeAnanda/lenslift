83cb3adba42d4e3d8822d2504f8cfcdd33ac0750
Gede Ananda <gdenand2020@gmail.com>
2026-06-07 14:36:53 +0700

    Initial Commit

---
+++ LensLift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LensLift
+//
+//  Created by I Gede Ananda Bela Persada on 07/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
